0d4bd8277ed91be6b622d9245000e22b1284a33e
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-08-30 18:18:28 +0200

    Updated the exercise with a faster solution

--- 009_palindrome_number.c
+++ 009_palindrome_number.c
@@ -3,117 +3,52 @@
 #include <stdbool.h>
 #include <string.h>
 
-int	ft_strlen(char *str)
+bool    ft_compare_half(int x)
 {
-	int	i = 0;
-	if (!str)
-		return 0;
-	while (str[i])
-		i++;
-	return i;
-}
+        int     half = 0;
+        int     modulo = 0;
 
-char	*ft_strdup(char *str)
-{
-	int i = 0;
-	char *line = malloc(sizeof(char) * ft_strlen(str) + 1);
-	if (!line)
-		return (NULL);
-	while (str[i])
-	{
-		line[i] = str[i];
-		i++;
-	}
-	line[i] = '\0';
-	return (line);
-}
-
-int	ft_strlen_n(int n)
-{
-	long int	len;
-	long int	n_temp;
-
-	len = 0;
-	n_temp = n;
-	if (n_temp < 0)
-	{	
-		len++;
-		n_temp *= -1;
-	}
-	while (n_temp != 0)
-	{
-		n_temp /= 10;
-		len++;
-	}
-	return (len);
-}
-
-
-char	*ft_itoa(int n)
-{
-	char		*array;
-	long int	i;
-	long int	n_temp;
-
-	n_temp = n;
-	i = ft_strlen_n(n);
-	if (n == 0)
-		return (ft_strdup("0"));
-	array = (char *)calloc(ft_strlen_n(n) + 1, sizeof(char));
-	if (!array)
-		return (NULL);
-	if (n_temp < 0)
-		n_temp *= -1;
-	while (i > 0)
-	{
-		array[i - 1] = n_temp % 10 + '0';
-		n_temp /= 10;
-		i--;
-	}
-	if (n < 0)
-		array[i] = '-';
-	return (array);
+        while (x > half)
+        {
+                half *= 10;
+                modulo = x % 10;
+                x = x / 10;
+                half += modulo;
+                if (half == x || half / 10 == x)
+                        return (true);
+        }
+        return (false);
 }
 
 bool isPalindrome(int x)
 {
-	int start = 0;
-	int end;
-	int half_size;
-	char	*str = ft_itoa(x);
-	if (!str)
-		return (false);
-	end = ft_strlen(str);
-	half_size = ft_strlen(str) / 2;
-	while (str[start] && start < half_size)
-	{
-		while (str[end - 1] && end >= 0)
-		{
-			if (str[start] == str[end - 1])
-			{
-				end--;
-				break ;
-			}
-			else
-				return (false);
-		}
-		start++;
-	}
-	return (true);
+    if (x < 0)
+        return (false);
+    else if (x == 0)
+            return (true);
+    else if (x % 10 == 0)
+            return (false);
+    else
+    {
+        if (ft_compare_half(x))
+                return (true);
+        else
+                return (false);
+    }
 }
 
-int	main(int ac, char **av)
+int     main(int ac, char **av)
 {
-	if (ac != 2)
-	{
-		printf("One argument required\n");
-		return (1);
-	}
-	int	test = atoi(av[1]);
-	int result = isPalindrome(test);
-	if (result == true)	
-		printf("true\n");
-	else if (result == false)
-		printf("false\n");
-	return (0);
+        if (ac != 2)
+        {
+                printf("One argument required\n");
+                return (1);
+        }
+        int     test = atoi(av[1]);
+        int result = isPalindrome(test);
+        if (result == true)
+                printf("true\n");
+        else if (result == false)
+                printf("false\n");
+        return (0);
 }
--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 | # | Title                                                                                  |             Solution                          | Difficulty | Topics                                    |
 | - | -------------------------------------------------------------------------------------- | --------------------------------------------- | ---------- | ----------------------------------------- |
 | 1 | [Two Sum](https://leetcode.com/problems/two-sum/)                                      | [C](001_two_sum.c)                            | Easy       | Array, Hash Table	                      |
-| 2 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/)                  | [C](009_palindrome_number.c)                  | Easy       |	                                      |
-| 3 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/)                  | [C](009_palindrome_number1.c) faster solution | Easy       | Math	                              |
+| 2 | [Palindrome Number](https://leetcode.com/problems/palindrome-number/)                  | [C](009_palindrome_number.c)                  | Easy       | Math                                      |
 | 4 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)                  | [C](020_valid_parentheses.c)                  | Easy       | String, Stack                             |
 | 5 | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/)                    | [C](013_roman_to_integer.c)                   | Easy       | Hash Table, Math, String                  |
 | 6 | [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)          | [C](014_longest_common_prefix.c)              | Easy       | Array, String, Trie                       |
