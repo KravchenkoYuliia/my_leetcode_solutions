7ba4dca2142cce2c2fa5235722f1ea30e00c0336
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-08-30 18:14:13 +0200

    Accepted on leetcode
    Runtime
    75ms
    Beats 52.46%
    
    Memory
    104.66MB
    Beats 6.56%
    Heavy on memory - reallocating memory for every new solution found

--- 301_remove_invalid_parentheses.c
+++ 301_remove_invalid_parentheses.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define TAKE 1
 #define NO 0
@@ -13,6 +14,17 @@ typedef struct s_par{
 	int	rm_close;
 	
 } t_par;
+
+typedef struct s_array {
+
+	char**	result;
+	char*	s;
+	int	i_of_solution;
+	int	size_of_malloc;
+	int	len_of_s;
+	int	len_of_result;
+
+} t_array;
 
 void	print_solution(char *s, int solution[])
 {
@@ -50,72 +62,159 @@ int	ft_nb_of_useless_par(char *s, t_par *par)
 	return (0);
 }
 
-
-
-void	get_solution(char *s, int index, int solution[], int open, int rm_o, int rm_cl, int len)
+int	malloc_place(t_array *array)
 {
-	if (index == len) //end, printing the result
+	if (array->i_of_solution == 0)
+	{
+		array->result = calloc(2, sizeof(char *));
+		if (!array->result)
+			return -1;
+		array->size_of_malloc = 2 * sizeof(char *);
+		array->result[array->i_of_solution] = calloc(array->len_of_result, sizeof(char));
+		if (!array->result[array->i_of_solution])
+			return -1;
+		array->result[array->i_of_solution + 1] = NULL;
+	}
+	else
+	{
+		array->result = realloc(array->result, array->size_of_malloc + sizeof(char *));
+		if (!array->result)
+			return -1;
+		array->size_of_malloc += sizeof(char *);
+		array->result[array->i_of_solution] = calloc(array->len_of_result, sizeof(char));
+		if (!array->result[array->i_of_solution])
+			return -1;
+		array->result[array->i_of_solution + 1] = NULL;
+	}
+	return 0;
+}
+
+void	check_duplicates(t_array *array)
+{
+	int	i = 0;
+
+	while (i < array->i_of_solution && array->result[i])
+	{
+		if (strcmp(array->result[i], array->result[array->i_of_solution]) == 0)
+		{
+			free(array->result[array->i_of_solution]);
+			array->result[array->i_of_solution] = NULL;
+			if (array->i_of_solution > 0)
+				array->i_of_solution--;
+			array->result = realloc(array->result, array->size_of_malloc - sizeof(char *));
+			return ;
+		}
+		i++;
+	}
+}
+
+void	save_solution(t_array *array, int solution[])
+{
+	int	i = 0;
+	int	j = 0;
+
+
+	if (malloc_place(array) == -1)
+		return ;
+	while (j < array->len_of_s)
+	{
+		if (solution[j] == TAKE)
+		{
+			array->result[array->i_of_solution][i] = array->s[j]; 
+			i++;
+		}
+		j++;
+	}
+	array->result[array->i_of_solution][i] = '\0';
+	check_duplicates(array);
+}
+
+void	get_solution(t_array *array, int index, int solution[], int open, int rm_o, int rm_cl)
+{
+	if (index == array->len_of_s) //end, printing the result
 	{
 		if (open == 0 && rm_o == 0 && rm_cl== 0)
-			print_solution(s, solution);
+		{
+			array->i_of_solution++;
+			save_solution(array, solution);
+		}
 		return ;
 	}
 
 
-	if (s[index] != '(' && s[index] != ')') //for other symbols always print
+	if (array->s[index] != '(' && array->s[index] != ')') //for other symbols always print
 	{
 		solution[index] = TAKE;
-		get_solution(s, index+1, solution, open, rm_o, rm_cl, len);
+		get_solution(array, index+1, solution, open, rm_o, rm_cl);
 		return ;
 	}
 
 
-	else if (s[index] == '(') //open parenthese
+	else if (array->s[index] == '(') //open parenthese
 	{
 		if (rm_o > 0) //need to remove any open 
 		{
 			solution[index] = NO;
-			get_solution(s, index+1, solution, open, rm_o-1, rm_cl, len);
+			get_solution(array, index+1, solution, open, rm_o-1, rm_cl);
 		}
 		solution[index] = TAKE;
-		get_solution(s, index+1, solution, open+1, rm_o, rm_cl, len);
-
+		get_solution(array, index+1, solution, open+1, rm_o, rm_cl);
 	}
-	else if (s[index] == ')') //close parenthese
+	else if (array->s[index] == ')') //close parenthese
 	{
 		if (open > 0)
 		{
 			solution[index] = TAKE;
-			get_solution(s, index+1, solution, open-1, rm_o, rm_cl, len);
+			get_solution(array, index+1, solution, open-1, rm_o, rm_cl);
 		}
 		if (rm_cl > 0)
 		{
 			solution[index] = NO;
-			get_solution(s, index+1, solution, open, rm_o, rm_cl-1, len);
+			get_solution(array, index+1, solution, open, rm_o, rm_cl-1);
 		}
 	}
 }
 
-void	removeInvalidParentheses(char* s)
+char**	removeInvalidParentheses(char* s, int* returnSize)
 {
-	t_par	par;
-	int	*solution;
-	int	index = 0;
-	int	open = 0;
-	int	len = strlen(s);
+	t_par		par;
+	t_array		array;
+	int		*solution;
+	int		index = 0;
+	int		open = 0;
 
-	solution = calloc(1000, sizeof(int));
+	array.result = NULL;
+	array.s = s;
+	array.size_of_malloc = 0;
+	array.len_of_s = strlen(s);
+	array.i_of_solution = -1;
+
+	solution = calloc(array.len_of_s, sizeof(int));
+	if (!solution)
+		return (NULL);
 	ft_nb_of_useless_par(s, &par);
-
-	get_solution(s, index, solution, open, par.rm_open, par.rm_close, len);
+	array.len_of_result = array.len_of_s - (par.rm_close + par.rm_open) + 1;
+	get_solution(&array, index, solution, open, par.rm_open, par.rm_close);
+	
+	*returnSize = array.i_of_solution+1;	
+	free(solution);
+	return(array.result);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac != 2)
 		return 1;
-	
-	removeInvalidParentheses(av[1]);
+	int	size = 0;
+	char**	res = removeInvalidParentheses(av[1], &size);
+	int i = 0;
+	while (res[i])
+	{
+		printf("%s\n", res[i]);
+		free(res[i]);
+		i++;
+	}
+	free(res);
 }
 
 
