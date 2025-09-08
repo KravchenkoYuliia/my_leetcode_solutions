0ccd8c3543b7bd44d599b3a0c449d363d4448bad
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-09-08 17:44:39 +0200

    Works
    Accepted on leetcode but I want to optimize it for minimize the runtime

--- 003_longest_substring_without_repeating_char.c
+++ 003_longest_substring_without_repeating_char.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+int	ft_check_double(char* s, char to_check, int current)
+{
+	while (current > 0)
+	{
+
+		if (to_check == s[current-1])
+			return 1;
+		current--;
+	}
+	return 0;
+}
+
+int	ft_result_from_current_substring(char* s)
+{
+	int	i = 0;
+	int	count = 0;
+	
+	while (s[i])
+	{
+		if (ft_check_double(s, s[i], i) == 1)
+			return count;
+		i++;
+		count++;
+	}
+
+	return count;
+}
+
 int lengthOfLongestSubstring(char* s)
 {
-    
+	int	i = 0;
+	int	result = 0;
+	int	substring_result;
+
+	while (s[i])
+	{
+		substring_result = ft_result_from_current_substring(&s[i]);
+		if (substring_result > result)
+			result = substring_result;
+		i++;
+	}
+
+	return result;
+
 }
 
 int	main(int ac, char** av)
