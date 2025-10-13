6693c3d9654525cc81992bcfb621fde957295a77
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2025-10-13 11:55:29 +0200

    feat: exercice is accepted on leetcode

--- 006_zigzag_conversion.c
+++ 006_zigzag_conversion.c
@@ -7,6 +7,7 @@ typedef struct s_data {
 	char*	src;
 	int	len;
 	int	nb_of_rows;
+	int	pattern_first_line;
 	int	nb_of_middle_rows;
 	int	start_of_middle_line;
 	char*	result;
@@ -19,6 +20,8 @@ void	ft_copy_last_line_of_zigzag(t_data* data)
 	int	i = data->nb_of_rows - 1;
 	int	pattern = data->nb_of_rows* 2 - 2;
 
+	if (data->i_result >= data->len)
+		return ;
 	while (data->src[i])
 	{
 		data->result[data->i_result] = data->src[i];
@@ -35,27 +38,29 @@ void	ft_copy_last_line_of_zigzag(t_data* data)
 
 }
 
-void	ft_copy_middle_lines_of_zigzag(t_data* data)
+void	ft_copy_middle_lines_of_zigzag(t_data* data, int pattern_a, int pattern_b)
 {
 	int	i;
 	int	line = 0;
 	int	count_char_in_middle_line = 0;
 	int	pattern = 0;
-	int	pattern_a = data->nb_of_rows * 2 - 4;
-	int	pattern_b = 2;
 
+	if (data->i_result >= data->len)
+		return ;
 	while (line < data->nb_of_middle_rows)
 	{
 		i = 0;
 		i = data->start_of_middle_line;
-		while (data->src[i])
+		count_char_in_middle_line = 0;
+		while (i < data->len && data->src[i])
 		{
+			if (data->i_result >= data->len)
+				return ;
 			data->result[data->i_result] = data->src[i];
 			if (count_char_in_middle_line % 2 == 0)
 				pattern = pattern_a;
 			else
 				pattern = pattern_b;
-
 			if (i + pattern < data->len && data->src[i + pattern])
 				i = i + pattern;
 			else
@@ -79,15 +84,23 @@ void	ft_copy_first_line_of_zigzag(t_data* data)
 	int	i = 0;
 	int	pattern = data->nb_of_rows* 2 - 2;
 
+	data->pattern_first_line = pattern;
 	while (data->src[i])
 	{
 		data->result[data->i_result] = data->src[i];
-		if (i + pattern  < data->len && data->src[i + pattern])
+		if (pattern == 0)
+			i++;
+		else if (i + pattern  < data->len && data->src[i + pattern])
 			i = i + pattern;
 		else
 		{
 			data->i_result++;
 			data->result[data->i_result] = '\0';
+			return ;
+		}
+		if (data->i_result >= data->len - 1 && data->nb_of_rows <= i + 1)
+		{
+			data->result[data->i_result + 1] = '\0';
 			return ;
 		}
 		data->i_result++;
@@ -97,14 +110,14 @@ void	ft_copy_first_line_of_zigzag(t_data* data)
 void	ft_convert_even_rows(t_data* data)
 {
 	ft_copy_first_line_of_zigzag(data);
-	ft_copy_middle_lines_of_zigzag(data);
+	ft_copy_middle_lines_of_zigzag(data, data->nb_of_rows * 2 - 4, 2);
 	ft_copy_last_line_of_zigzag(data);
 }
 
 void	ft_convert_odd_rows(t_data* data)
 {
 	ft_copy_first_line_of_zigzag(data);
-	//ft_copy_middle_lines_of_zigzag_odd(data);
+	ft_copy_middle_lines_of_zigzag(data, data->pattern_first_line - 2, 2);
 	ft_copy_last_line_of_zigzag(data);
 }
 
@@ -122,6 +135,11 @@ char*	convert(char* s, int numRows)
 	if (!data.result)
 		return NULL;
 	
+	if (data.nb_of_rows == 1)
+	{
+		strcpy(data.result, s);
+		return data.result;
+	}
 	if (numRows % 2 == 0)
 		ft_convert_even_rows(&data);
 	else
