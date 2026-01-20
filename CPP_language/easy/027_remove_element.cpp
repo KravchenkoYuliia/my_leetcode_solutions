51e2d55aadd41977715145336bbb115efd696bd4
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-01-20 14:10:02 +0100

    feat: start of the new assignement - easy - remove element in C++

---
+++ CPP_language/easy/027_remove_element.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   027_remove_element.cpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/20 14:08:17 by yukravch          #+#    #+#             */
+/*   Updated: 2026/01/20 14:08:17 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> 
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+    }
+};
+
+int	main(int ac, char** av) {
+
+	(void)ac;
+	(void)av;
+	return 0;
+}
