7114ae5210971354caedd4d531b8311a203375e3
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-01-23 12:19:44 +0100

    feat: implement Dijkstra's algorithm

--- CPP_language/medium/network_delay_time/743_network_delay_time.cpp
+++ CPP_language/medium/network_delay_time/743_network_delay_time.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   743_network_delay_time.cpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
+/*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/22 14:27:14 by yukravch          #+#    #+#             */
-/*   Updated: 2026/01/22 14:27:14 by yukravch         ###   ########.fr       */
+/*   Updated: 2026/01/23 11:19:28 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,68 +23,7 @@ k -> source node - from where the signal is send
 using namespace std;
 class Solution {
 
-public:// TEMP METHODS
-	/*static void		printVectorOfVector( vector< vector<int> >& v ) {
-			
-		for ( int i = 0; i < v.size(); i++ ) {
-			
-			std::cout << "[";
-			for ( int j = 0; j < v[i].size(); j++ ) {
-				
-				std::cout << v[i][j];
-				if ( j < v[i].size() - 1 )
-					std::cout << ",";
-			}
-			std::cout << "]" << std::endl;
-		}
-
-	}
-	*/
-	/*static void	printAdjacencyList( vector< vector< pair<int,int> > >&  adj ) {
-
-		for ( int i = 0; i < adj.size(); i++ ) {
-
-			cout << "adj[" << i << "]";
-			if ( adj[i].empty() ) {
-				cout << "()" << endl;
-			}
-			else {
-			
-				for ( int j = 0; j < adj[i].size(); j++ ) {
-
-					cout << "(" << adj[i][j].first << "," << adj[i][j].second << ")";
-				}
-				cout << endl;
-			}
-		
-		}
-	}*/
-	/*
-	static void	printDistanceTable( vector< int >& v ) {
-
-		for ( int i = 0; i < v.size(); i++ ) {
-
-			cout << "from source to the node [" << i << "]: " << v[i] << endl; 
-		}
-	}
-	*/
-	static void	makeAdjacencyList( vector< vector< pair<int,int> > >&  adj, vector< vector<int> >& v ) {
-
-		for ( int i = 0; i < v.size(); i++ ) {
-		
-				adj[ v[i][0] ].push_back( make_pair( v[i][1], v[i][2] ) );
-		}
-		
-	}
-
-	static void	makeDistanceTable( vector< int >& distanceTable, int k ) {
-
-		for ( int i = 0; i < distanceTable.size(); i++ ) {
-
-			distanceTable[i] = INT_MAX;
-		}
-		distanceTable[k] = 0;
-	}
+public:
 
 	static int	networkDelayTime( vector< vector<int> >& times, int n, int k) {
 	
@@ -92,29 +31,62 @@ public:// TEMP METHODS
 		vector< int >				distanceTable( n + 1 );
 		priority_queue< pair< int,int > >	pq;
 
-		Solution::makeAdjacencyList( adj, times );
-		Solution::makeDistanceTable( distanceTable, k );
+		for ( int i = 0; i < times.size(); i++ ) {
 		
+			adj[ times[i][0] ].push_back( make_pair( times[i][1], times[i][2] ) );
+		}
+
+		for ( int i = 0; i < distanceTable.size(); i++ ) {
+
+			distanceTable[i] = INT_MAX;
+		}
+		distanceTable[k] = 0;
+
 		pq.push( make_pair( 0, k ) );
-		cout << pq.top().first << " and " << pq.top().second;
-		
-		return -1;
+		while ( !pq.empty() ) {
+			int	currentNode = pq.top().second;
+			int	currentDistance = pq.top().first;
+			if ( currentDistance > distanceTable[ currentNode ])
+				continue;
+				
+			pq.pop();
+			
+			for ( int i = 0; i < adj[currentNode].size(); i++ ) {
+				int	neighborOfCurrentNode = adj[currentNode][i].first;
+				int	delayTime = distanceTable[currentNode] + adj[currentNode][i].second;
+
+				if ( delayTime < distanceTable[ neighborOfCurrentNode ] ) {
+					distanceTable[ neighborOfCurrentNode ] = delayTime;
+					pq.push( make_pair( distanceTable[ neighborOfCurrentNode ], neighborOfCurrentNode ) );
+					
+				}
+			}
+		}
+
+		int	maxTime = 0;
+		for ( int i = 1; i < distanceTable.size(); i++ ) {
+
+			if ( maxTime < distanceTable[ i ] )
+				maxTime = distanceTable[ i ];
+		}
+
+		if ( maxTime == INT_MAX )
+			return -1;
+		return maxTime;
 	}
-
-
 };
 
 int	main( void ) {
 
 	vector< vector<int> >	v = {
+
 		{2,1,1},
 		{2,3,1},
 		{3,4,1}
 	};
 	
 	int	result = Solution::networkDelayTime( v, 4, 2 );
-	//cout << "Result = " << result << std::endl;
+	cout << "Result = " << result << std::endl;
 
 	return 0;
 }
-
--- CPP_language/medium/network_delay_time/README.md
+++ CPP_language/medium/network_delay_time/README.md
@@ -17,6 +17,7 @@
 
 	initialization:
 			index		distance
+			-------------------------------
 			1		INT_MAX
 			2		0 ( source node )
 			3		INT_MAX
@@ -30,10 +31,10 @@
 	start: push ( 0, k ) to the queue because source node send the signal
 		get the top value of the priority queue to check the neighbors of the current node
 
-		current node is `2`
+		current node is `k=2`
 		take its neighbors from the adjacence list: `2` has `1` and `3` ( adj[2][0].first and adj[2][1].first )
-		the signal from the `2` can is spreading to `1` and `3`
-		calculate the distance to the them:
+		the signal from the `2` is spreading to `1` and to `3`
+		calculate the distance to them:
 						new distanceTable[1] = distanceTable[2] + adj[2][0].second = 0 + 1 = 1;
 						if this value < the actual distanceTable[1]: write it down 
 						else: ignore
@@ -42,20 +43,87 @@
 						1 < INT_MAX
 						distanceTable[3] = 1
 
+push new candidants to the queue:
+				1,`1`
+				1,`3`
+take any node ( order is not important ) and repeate the same steps for `1` and `3` and so on
+		if the node we take from the priority queue is already visited - ignore - top the next one
+
 ### 4 mark the node as visited after checking his heighbors
 
 	vector< bool >
 	
 	node=index		status
+	--------------------------------
 	1			false
 	2			visited
 	3			false
 	4			false
 
 
-	push new candidants to the queue:
-					1,`1`
-					1,`3`
-	take any node ( order is not important ) and repeate the same steps for `1` and `3` and so on
-		if the node we take from the priority queue is already visited - ignore - top the next one
-				
+nput: times = [[1,2,1]], n = 2, k = 1
+Output: 1
+
+Example 3:
+
+Input: times = [[1,2,1]], n = 2, k = 2
+Output: -1
+
+## TEST
+### 1
+	vectors:{2,1,1},
+		{2,3,1},
+		{3,4,1}
+	n = 4
+	k = 2
+
+	Expected result : 2
+### 2
+	vectors:{1,2,1}
+	n = 2
+	k = 1
+
+	Expected result: 1
+
+### 3
+	vectors:{1,2,1}
+	n = 2
+	k = 2
+	
+	Expected result: -1
+
+### 4
+	vectors:{1,2,1},
+		{2,1,3}
+	n = 2
+	k = 2
+	
+	Expected result: 3
+
+### 5
+	vectors:{{4,2,76},
+	{1,3,79},
+	{3,1,81},
+	{4,3,30},
+	{2,1,47},
+	{1,5,61},
+	{1,4,99},
+	{3,4,68},
+	{3,5,46},
+	{4,1,6},
+	{5,4,7},
+	{5,3,44},
+	{4,5,19},
+	{2,3,13},
+	{3,2,18},
+	{1,2,0},
+	{5,1,25},
+	{2,5,58},
+	{2,4,77},
+	{5,2,74}}
+
+	n = 5
+	k = 3
+	
+	Expected result: 59
+
