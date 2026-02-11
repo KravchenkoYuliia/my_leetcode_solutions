b3b8b5748f42179b546b862fdc715cf475547cce
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-02-11 15:32:51 +0100

    solution found

--- CPP_language/easy/028_find_the_index_of_the_first_occurence_in_a_string.cpp
+++ CPP_language/easy/028_find_the_index_of_the_first_occurence_in_a_string.cpp
@@ -6,13 +6,49 @@
 /*   By: yukravch <yukravch@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 14:35:28 by yukravch          #+#    #+#             */
-/*   Updated: 2026/02/11 14:35:28 by yukravch         ###   ########.fr       */
+/*   Updated: 2026/02/11 15:28:18 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        
+    static int		strStr( string haystack, string needle ) {
+	
+	    int	needleSize = needle.size();
+	    
+		for ( int i = 0; i < haystack.size(); i++ ) {
+			
+			if ( haystack[ i ] == needle[ 0 ] ) {
+
+				int start = i;
+				int stop = start + needleSize;
+
+				for ( int j = 0; j < needleSize; j++ ) {
+
+					if ( start < stop && haystack[ start ] != needle[ j ] )
+						break;
+					if ( start == stop - 1 )
+						return i;
+					start++;
+				}
+
+			}
+		}
+	   
+
+        return -1;
     }
 };
+
+
+int	main( int ac, char** av ) {
+
+	if ( ac != 3 ) {  cerr << "./a.out \"where\" \"what\"" << endl; return 1;  }
+
+	cout << Solution::strStr( av[1], av[2] ) << endl;
+
+	return 0;
+}
