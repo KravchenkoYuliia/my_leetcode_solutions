0589b11c4b902507e6c12b7da9be40a9cc98e6af
KravchenkoYuliia <yuliiakravchenko10@gmail.com>
2026-02-11 16:18:55 +0100

    add main function

--- CPP_language/medium/container_with_most_water/011_container_with_most_water.cpp
+++ CPP_language/medium/container_with_most_water/011_container_with_most_water.cpp
@@ -10,4 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-s
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    static int maxArea( vector< int >& height ) {
+     
+	    for ( int i = 0; i < height.size(); i++ )
+		    cout << height[i] << endl;
+    	return -1;
+    }
+};
+
+int	main( int ac, char** av ) {
+
+	if ( ac != 2 ) {
+		cerr << "Write 1 arg with heights separated by spaces" << endl;
+		return 1;
+	}
+
+	vector< int >	height;
+	for ( int i = 0; av[1][i]; i++ ) {
+
+		if ( av[1][i] != ' ')
+			height.push_back( av[1][i] - '0' );
+	}
+
+	cout << Solution::maxArea( height ) << endl;
+	return 0;
+}
---
+++ CPP_language/medium/container_with_most_water/README.md
@@ -0,0 +1,15 @@
+# Greedy Algorithm
+
+- is a class of algorithms that make locally optimal choices at each step that looks the best at the moment
+-  do not always give the best solution. For example, in coin change and 0/1 knapsack problems - the best solution is Dynamic Programing
+
+Examples:
+
+- Fractional Knapsack
+- Dijkstra's algorithm 
+- Kruskal's algorithm
+- Huffman coding
+- Prim's Algorithm
+
+## Dijkstra's algorithm
+	
